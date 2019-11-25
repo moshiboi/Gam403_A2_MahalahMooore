5dfe63d7cf79ef8815da2346f8dcae9210e369fe
moshiboi <syncedidentity@gmail.com>
2019-11-25 16:12:39 +1100

    Updates
    Added:
    Pause + pause menu
    Pickups
    Mini map
    Fading pickup overlay
    Updated maze visuals to look nicer

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -13,6 +13,12 @@ public class Player_Controller : MonoBehaviour
     float moveHorizontal;
     float moveVertical;
 
+    public GameObject Pickup, Pickup1, Pickup2, Pickup3, Pickup4;
+        public GameObject Plane, Plane1, Plane2, Plane3, Plane4;
+    public GameObject Menu;
+
+    bool paused;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -21,6 +27,24 @@ public class Player_Controller : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused == true)
+            {
+                Time.timeScale = 0;
+                paused = false;
+                        Menu.SetActive(true);
+            }
+
+            else if (paused == false)
+            {
+                Time.timeScale = 1;
+                paused = true;
+                Menu.SetActive(false);
+            }
+            
+        }
+
         //moveHorizontal = Input.GetAxis("Horizontal");
         //moveVertical = Input.GetAxis("Vertical");
         //_________________________________________________
@@ -33,11 +57,52 @@ public class Player_Controller : MonoBehaviour
         moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        Vector3 newMovement = Vector3.ClampMagnitude(movement *speed, speed);
+        Vector3 newMovement = Vector3.ClampMagnitude(movement * speed, speed);
 
         rb.AddForce(newMovement);
         //physics updates are better here
         //fixed update updates at specific moments and is more consistent
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            if (other.gameObject == Pickup)
+            {
+                StartCoroutine(Timer(Plane, 5));
+            }
+
+            if (other.gameObject == Pickup1)
+            {
+                StartCoroutine(Timer(Plane1, 5));
+            }
+
+            if (other.gameObject == Pickup2)
+            {
+                StartCoroutine(Timer(Plane2, 5));
+            }
+
+            if (other.gameObject == Pickup3)
+            {
+                StartCoroutine(Timer(Plane3, 5));
+            }
+
+            if (other.gameObject == Pickup4)
+            {
+                StartCoroutine(Timer(Plane4, 5));
+            }
+
+
+            other.gameObject.SetActive(false);
+            print("Pickup");
+        }
+    }
+
+    IEnumerator Timer (GameObject Obj,float time)
+    {
+        Obj.SetActive(true);
+        yield return new WaitForSeconds(time);
+        Obj.SetActive(false);
+    }
 }
